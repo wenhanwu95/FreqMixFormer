0ab6d908e2367d8e110530394239ac246aae641b
5wh <wenhanwu95@gmail.com>
2024-11-10 11:59:30 -0500

    Update ensemble.sh

--- ensemble.sh
+++ ensemble.sh
@@ -1,5 +1,5 @@
-# Description: Ensemble the predictions of different models.
-# Example: bash ensemble.sh
+# Ensemble the predictions of different models.
+# Example of NTU60 CS setting
 
 ###################  60cs   ################### 
 python ensemble.py --dataset ntu/xsub  \
@@ -7,7 +7,7 @@ python ensemble.py --dataset ntu/xsub  \
 --bone-dir  work_dir/ntu/csub/skmixf_8motion \
 --joint-motion-dir work_dir/ntu/csub/skmixf_1joint \
 --bone-motion-dir work_dir/ntu/csub/skmixf_1motion \
---joint-k2-dir work_dir/ntu/csub/skmixf_2joint\
+--joint-k2-dir work_dir/ntu/csub/skmixf_2joint \
 --joint-motion-k2-dir  work_dir/ntu/csub/skmixf_2motion
 
 
