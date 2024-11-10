b4076427562b61311989804d6c8479d8f4b15ad0
5wh <wenhanwu95@gmail.com>
2024-11-10 11:58:18 -0500

    Update evaluate.sh

--- evaluate.sh
+++ evaluate.sh
@@ -1,8 +1,8 @@
-# Description: Evaluate the model on NTU60 dataset.
-# Example: bash evaluate.sh on NTU60 dataset 
+# Example: Evaluate the model on NTU60 dataset.
+# Change the commands depending on your evaluation
 
 python main.py \
---config work_dir/ntu/csub/skmixf_241/config.yaml \
---work-dir work_dir/ntu/csub/skmixf_241 --phase test --save-score True \
---weights work_dir/ntu/csub/skmixf_241/runs-95-29735.pt 
+--config work_dir/ntu/csub/skmixf/config.yaml \
+--work-dir work_dir/ntu/csub/skmixf --phase test --save-score True \
+--weights work_dir/ntu/csub/skmixf/runs-95-29735.pt 
 --device 0
