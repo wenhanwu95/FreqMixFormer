ace5bae449bab7fdedcfd4145c4d5e0849cfc69a
5wh <wenhanwu95@gmail.com>
2024-11-10 12:02:58 -0500

    Update ensemble.sh

--- ensemble.sh
+++ ensemble.sh
@@ -3,12 +3,12 @@
 
 ###################  60cs   ################### 
 python ensemble.py --dataset ntu/xsub  \
---joint-dir  work_dir/ntu/csub/skmixf_8joint \
---bone-dir  work_dir/ntu/csub/skmixf_8motion \
---joint-motion-dir work_dir/ntu/csub/skmixf_1joint \
---bone-motion-dir work_dir/ntu/csub/skmixf_1motion \
---joint-k2-dir work_dir/ntu/csub/skmixf_2joint \
---joint-motion-k2-dir  work_dir/ntu/csub/skmixf_2motion
+--joint-dir  work_dir/ntu/csub/skfreq_8joint \
+--bone-dir  work_dir/ntu/csub/skfreq_8motion \
+--joint-motion-dir work_dir/ntu/csub/skfreq_1joint \
+--bone-motion-dir work_dir/ntu/csub/skfreq_1motion \
+--joint-k2-dir work_dir/ntu/csub/skfreq_2joint \
+--joint-motion-k2-dir  work_dir/ntu/csub/skfreq_2motion
 
 
 
